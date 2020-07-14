be843c6dfb6653bb69c1fecac37d11c4aa6688ee
Andrew <andrew8gmf@gmail.com>
2020-07-14 20:17:59 -0300

    add date model

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -17,6 +17,7 @@ module.exports = {
                 email,
                 username,
                 password,
+                createdAt,
             });
         };
 
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  username: String,
-  password: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
