1808af0ce3f6ba5653fc7330a638723f7902f664
Andrew <andrew8gmf@gmail.com>
2020-07-28 17:50:20 -0300

    react structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
+import Routes from "./routes";
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
-
+const App = () => <Routes />;
 export default App;
