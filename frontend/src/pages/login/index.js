c3d23ce66b4fce025dd6cd12c2dc1fdac1086102
Andrew <andrew8gmf@gmail.com>
2020-07-28 20:12:22 -0300

    login working

---
+++ frontend/src/pages/login/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+import api from "../../services/api";
+import { login } from "../../services/auth";
+
+import { Form, Container } from "./styles";
+
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    error: ""
+  };
+
+  handleLogin = async e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    if (!email || !password) {
+      this.setState({ error: "Preencha e-mail e senha para continuar!" });
+    } else {
+      try {
+        const response = await api.post("/login", { email, password });
+        login(response.data.token);
+        this.props.history.push("/app");
+      } catch (err) {
+        this.setState({
+          error:
+            "Houve um problema com o login, verifique suas credenciais."
+        });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.handleLogin}>
+          {this.state.error && <p>{this.state.error}</p>}
+          <input
+            type="email"
+            placeholder="Endereço de e-mail"
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <button type="submit">Entrar</button>
+          <hr />
+          <Link to="/signup">Criar conta grátis</Link>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default withRouter(Login);
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./services/auth";
 
 import Register from "./pages/register";
+import Login from "./pages/login";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -22,7 +23,7 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <h1>Welcome</h1>} />
-      <Route path="/login" component={() => <h1>Login</h1>} />
+      <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <PrivateRoute path="/app" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
