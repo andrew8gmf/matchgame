3ad5d74d2b4c8fc87dba860864f3c56c6327ef0e
Andrew <andrew8gmf@gmail.com>
2020-07-28 18:14:45 -0300

    add register page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import { isAuthenticated } from "./services/auth";
 
+import Register from "./pages/register";
+
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
@@ -21,7 +23,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={() => <h1>Welcome</h1>} />
       <Route path="/login" component={() => <h1>Login</h1>} />
-      <Route path="/register" component={() => <h1>Register</h1>} />
+      <Route path="/register" component={Register} />
       <PrivateRoute path="/app" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
